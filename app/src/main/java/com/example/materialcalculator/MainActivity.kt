f1399ed33b3f402121d46df3994f64e6f1adba9b
Hussain Gaddal <hussain.yg2010@gmail.com>
2026-05-09 01:12:38 +0300

    Enable edge-to-edge in MainActivity

--- app/src/main/java/com/example/materialcalculator/MainActivity.kt
+++ app/src/main/java/com/example/materialcalculator/MainActivity.kt
@@ -3,19 +3,14 @@ package com.example.materialcalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.enableEdgeToEdge
 import com.example.materialcalculator.presentation.CalculatorScreen
 import com.example.materialcalculator.ui.theme.MaterialCalculatorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             MaterialCalculatorTheme {
                 CalculatorScreen()
